249458a82705e923e68aa6c40306ff2f018f79f1
DongJu-Na <ehdeld123@naver.com>
2023-03-02 18:47:21 +0900

    init
    init

---
+++ src/test/java/com/example/springGpt/SpringGptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springGpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
